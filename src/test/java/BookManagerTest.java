8b35bcfc5888649efab1b020772b175fac73d96b
GianNaufal-web <bastraddruig@gmail.com>
2025-09-01 21:33:16 +0700

    FIRST COMMIT

---
+++ src/test/java/BookManagerTest.java
@@ -0,0 +1,55 @@
+import org.example.Book;
+import org.example.BookManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class BookManagerTest {
+    private BookManager bookManager;
+
+    @BeforeEach
+    void setUp() {
+        bookManager = new BookManager();
+    }
+
+    @Test
+    @DisplayName("Test menambahkan buku")
+    void testAddBook() {
+        Book buku = new Book("Pemrograman", "Andi", 2020);
+        bookManager.addBook(buku);
+        assertEquals(1, bookManager.getBookCount());
+    }
+
+    @Test
+    @DisplayName("Test menghapus buku yang ada")
+    void testRemoveExistingBook() {
+        Book buku = new Book("Basis Data", "Erlangga", 2021);
+        bookManager.addBook(buku);
+
+        boolean removed = bookManager.removeBook("Basis Data");
+        assertTrue(removed);
+        assertEquals(0, bookManager.getBookCount());
+    }
+
+    // Lengkapi Unit Test dibawah untuk buku yang memang tidak terdapat pada List
+    @Test
+    @DisplayName("Test menghapus buku yang tidak ada")
+    void testRemoveNonExistingBook() {
+    }
+
+    // Lengkapi Unit Test dibawah untuk mencari buku berdasarkan penulis
+    @Test
+    @DisplayName("Test mencari buku berdasarkan author")
+    void testFindBooksByAuthor() {
+    }
+
+    // Lengkapi Unit Test dibawah untuk seluruh buku yang ada di dalam list
+    @Test
+    @DisplayName("Test mendapatkan semua buku")
+    void testGetAllBooks() {
+    }
+}
